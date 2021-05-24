b9e57334bcbc5a2de6906b0c49e7f2e4215735ed
ilanribeiro <ilanribeiro@gmail.com>
2021-05-24 18:03:04 -0300

    Inclui botão de restart do jogo

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ let canvas = document.querySelector("#snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let points = 0;
+let restartButton = document.querySelector('.restart-button');
 let snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 }
+
 let direction = "right";
 let food = {
   x: Math.floor(Math.random() * 16) * box,
   y: Math.floor(Math.random() * 16) * box,
+}
+
+document.addEventListener('keydown', update);
+restartButton.addEventListener('click', reloadPage);
+
+
+function button() {
+  restartButton.classList.toggle('active');
+}
+
+function reloadPage() {
+  location.reload();
 }
 
 function buildBG() {
@@ -20,7 +34,7 @@ function buildBG() {
 
 function buildSnake() {
   for (let index = 0; index < snake.length; index++) {
-    context.fillStyle = "green";
+    context.fillStyle = "blue";
     context.fillRect(snake[index].x, snake[index].y, box, box);
   }
 }
@@ -30,8 +44,6 @@ function drawFood() {
   context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
-
 function update (event) {
   if(event.key === 'ArrowLeft' && direction !== 'right') direction = 'left';
   if(event.key === 'ArrowRight' && direction !== 'left') direction = 'right';
@@ -39,19 +51,36 @@ function update (event) {
   if(event.key === 'ArrowDown' && direction !== 'up') direction = 'down';
 }
 
-function startGame() {
+function defineTransferPosition() {
   if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
   if(snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
   if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
   if(snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
+}
 
+function ruleEatFood(snakX, snakY) {
+  if(snakX !== food.x || snakY !== food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 16) * box;
+    food.y = Math.floor(Math.random() * 16) * box;
+    points += 10;
+  }
+}
+
+function ruleToEndGame() {
   for(let index = 1; index < snake.length; index++) {
     if(snake[0].x === snake[index].x && snake[0].y === snake[index].y) {
       clearInterval(game);
       alert(`Fim de Jogo. você fez ${points} pontos`);
+      button();
     }
   }
-  
+}
+
+function startGame() {
+  defineTransferPosition();
+  ruleToEndGame();
   buildBG();
   buildSnake();
   drawFood();
@@ -64,15 +93,8 @@ function startGame() {
   if(direction == 'up') snakeY -= box;
   if(direction == 'down') snakeY += box;
 
-  if(snakeX !== food.x || snakeY !== food.y) {
-    snake.pop();
-  } else {
-    food.x = Math.floor(Math.random() * 16) * box;
-    food.y = Math.floor(Math.random() * 16) * box;
-    points += 10;
-  }
+  ruleEatFood(snakeX, snakeY);  
   
-
   let newHead = {
     x: snakeX,
     y: snakeY,
