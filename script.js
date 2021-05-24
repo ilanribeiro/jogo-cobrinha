693c4387e62c8ed91d94ec319e78c0a2db94874b
ilanribeiro <ilanribeiro@gmail.com>
2021-05-24 07:15:28 -0300

    Cria regras de movimento da cobrinha

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box,
 }
+let direction = "left";
 
-function criarBG() {
+function buildBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha() {
+function buildSnake() {
   for (let index = 0; index < snake.length; index++) {
     context.fillStyle = "green";
     context.fillRect(snake[0].x, snake[0].y, box, box);
   }
 }
 
-criarBG();
-criarCobrinha();
+function startGame() {
+  buildBG();
+  buildSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == 'right') snakeX += box;
+  if(direction == 'left') snakeX -= box;
+  if(direction == 'up') snakeY -= box;
+  if(direction == 'down') snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  }
+
+  snake.unshift(newHead);
+} 
+
+let game = setInterval(startGame, 100);
